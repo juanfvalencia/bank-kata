4a64e377c94990395ec07773a45f5db410610869
juanfvalencia <90592176+juanfvalencia@users.noreply.github.com>
2021-11-29 17:57:10 -0500

    Se agregan los metodos decimalFormatter, saveCreditLine ysaveDebitLine en la clase StatementLine

--- src/main/java/com/sofka/bank/StatementLine.java
+++ src/main/java/com/sofka/bank/StatementLine.java
@@ -1,4 +1,28 @@
 package com.sofka.bank;
 
+import com.sofka.bank.Statement;
+
+import java.text.DecimalFormat;
+
 public class StatementLine {
+
+    private Statement statement = new Statement();
+
+    public StatementLine() {
+    }
+
+    public String decimalFormatter(double amount){
+        DecimalFormat df = new DecimalFormat("########.00");
+        return df.format(amount);
+    }
+
+    public void saveCreditLine(String date, double credit, double balance) {
+        String line = ("|" + date + "| " + decimalFormatter(credit) + " |        " + "  |  " + decimalFormatter(balance) + "   |");
+        statement.addStatementLine(line);
+    }
+
+    public void saveDebitLine(String date, double debit, double balance) {
+        String line = ("|" + date + "| " + "        |  " + decimalFormatter(debit) + "  |  " + decimalFormatter(balance) + "   |");
+        statement.addStatementLine(line);
+    }
 }
