09f158740a2f9826735ba65cc3121fb3fc30d77f
juanfvalencia <90592176+juanfvalencia@users.noreply.github.com>
2021-11-29 17:54:31 -0500

    Se agregan los metodos credit y debir en la clase Transaction

--- src/main/java/com/sofka/bank/Transaction.java
+++ src/main/java/com/sofka/bank/Transaction.java
@@ -1,4 +1,25 @@
 package com.sofka.bank;
 
+import com.sofka.bank.StatementLine;
+
 public class Transaction {
+
+    private double balance = 0;
+    StatementLine statementLine = new StatementLine();
+
+    public double amountOf(double amount) {
+        return amount;
+    }
+
+    public double credit(double amount, String date) {
+        this.balance = this.balance + amount;
+        statementLine.saveCreditLine(date, amount, this.balance);
+        return this.balance;
+    }
+
+    public double debit(double amount, String date) {
+        this.balance = this.balance - amount;
+        statementLine.saveDebitLine(date, amount, this.balance);
+        return this.balance;
+    }
 }
