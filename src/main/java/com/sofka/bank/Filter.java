213053bc7b58666813027519d5a4c9a862a132dd
juanfvalencia <90592176+juanfvalencia@users.noreply.github.com>
2021-11-29 17:59:26 -0500

    Se agregan los metodos printFilterByDate, printOnlyCredit y printOnlyDebit en la clase Filter

--- src/main/java/com/sofka/bank/Filter.java
+++ src/main/java/com/sofka/bank/Filter.java
@@ -1,4 +1,41 @@
 package com.sofka.bank;
 
+import java.io.PrintStream;
+import java.util.List;
+
 public class Filter {
+
+    private Statement statement = new Statement();
+    private List<String> statementList = statement.returnStatementlist();
+
+    public void printFilterByDate(PrintStream printer, String filter){
+        System.out.println("\n---FILTER BY DATE---");
+        statement.statementTitle(System.out);
+        for(int i = 0; i < statementList.size(); i++){
+            String stringAtIndex = statementList.get(i);
+            String subStringLine = stringAtIndex.substring(1,11);
+            if(subStringLine.equals(filter)) printer.println(statementList.get(i));
+        }
+    }
+
+    public void printOnlyCredit(PrintStream printer){
+        System.out.println("\n FILTER BY CREDIT");
+        statement.statementTitle(System.out);
+        for(int i = 0; i < statementList.size(); i++){
+            String stringAtIndex = statementList.get(i);
+            char subChar = stringAtIndex.charAt(13);
+            if(subChar != ' ') printer.println(statement.statementList.get(i));
+        }
+    }
+
+    public void printOnlyDebit(PrintStream printer){
+        System.out.println("\n FILTER BY DEBIT");
+        statement.statementTitle(System.out);
+        for(int i = 0; i < statementList.size(); i++){
+            String stringAtIndex = statementList.get(i);
+            char subChar = stringAtIndex.charAt(26);
+            if(subChar != ' ') printer.println(statement.statementList.get(i));
+        }
+    }
+
 }
